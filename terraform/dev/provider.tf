0c9a0514ea3532756f99ed0f0cc63b63de53388e
prashantmukadam16 <prashantmukadam16@gmail.com>
2026-05-27 20:10:40 +0530

    correct the credential name

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("terraform-key.json")
+  credentials = file("gcp-key.json")
   project     = var.project_id
   region      = var.region
   zone        = var.zone
