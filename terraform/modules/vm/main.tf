413e93501aa3f63a80441fc0a2b95d0bff085b04
prashantmukadam16 <prashantmukadam16@gmail.com>
2026-05-27 19:21:13 +0530

    change the ubunti version 2204 to 2404

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts"
     }
   }
 
